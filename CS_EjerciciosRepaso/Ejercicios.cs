d3efc8e146f40867ebe492032e1b7522501a794c
mnardelli16 <maximiliano.nardelli@gmail.com>
2020-09-04 13:56:41 -0300

    Ejercicio 1
    Se agrega al resolucion del ejercicio 1

---
+++ CS_EjerciciosRepaso/Ejercicios.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_EjerciciosRepaso
+{
+    class Ejercicios
+    {
+        public void Ejercicio1()
+        {
+            string StrRegistro;
+            bool flag = false;
+            int Registro = 0;
+
+            do
+            {
+                Console.WriteLine("Por favor ingrese Numero de Registro");
+                StrRegistro = Console.ReadLine();
+
+                flag = ValidarNumero(StrRegistro, ref Registro);
+
+            } while (flag == false);
+
+            string NuevoRegSTR = Registro.ToString().Substring(3, 3);
+            int NuevoReg = 0;
+
+            if (NuevoRegSTR.Substring(0, 1) == "0")
+            {
+                int R = Convert.ToInt32(NuevoRegSTR);
+                NuevoReg = R + 100;
+                Console.WriteLine("El nuevo numero de registro es: {0}", NuevoReg);
+            }
+
+
+            for( int i = 1; i <= NuevoReg; i++ )
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("{0} - FooBar", i);
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("{0} - Foo", i);
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("{0} - Bar", i);
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        private bool ValidarNumero(string NroRegistro, ref int Registro)
+        {
+            bool flag = false;
+
+            if(!Int32.TryParse(NroRegistro, out Registro))
+            {
+                Console.WriteLine("Debe ingresar un numero");
+            }
+            else if (Registro <= 0)
+            {
+                Console.WriteLine("Debe ingresar un numero positivo");
+            }
+            else
+            {
+                flag = true;
+            }
+            return flag;
+        }
+
+    }
+}
--- CS_EjerciciosRepaso/Program.cs
+++ CS_EjerciciosRepaso/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +11,14 @@ namespace CS_EjerciciosRepaso
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HolaMUndo!");
+            Ejercicios E = new Ejercicios();
+
+            E.Ejercicio1();
+
+
 
             Console.ReadKey();
+         
         }
     }
 }
